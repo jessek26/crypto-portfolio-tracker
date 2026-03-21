b990cdb87fd0632664c459870b1cf90c1662df21
jessek26 <jesskooyenga@gmail.com>
2026-03-20 21:32:38 -0400

    added auth routes and dotenv configuration

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,15 +5,17 @@ const { User } = require('../models/index');
 
 const router = express.Router();
 
+//register route
 router.post('/register', async (req,res) => {
+    try {
     const { email, password } = req.body;
     
     //looks for the email already being used by a user
-    const exisitingUser = await User.findOne({ where: { email }});
+    const existingUser = await User.findOne({ where: { email }});
 
     //if the email is already in use
-    if(exisitingUser) {
-        return res.status(400).json({error: 'email in use'})
+    if(existingUser) {
+        return res.status(400).json({error: 'email in use'});
     }
 
     //using bcrypt to has password w/ 10 salt rounds 
@@ -21,4 +23,39 @@ router.post('/register', async (req,res) => {
 
     //waits for user to be created in the database 
     const user = await User.create({ email, password: hashedPassword })
+
+    //creates a token for the user
+    const token = jsonwebtoken.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+    res.status(201).json({ token: token })
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' })
+    }
 })
+
+//login route
+router.post('/login', async (req, res)  => {
+    try{
+        const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ where: { email }});
+        if(!existingUser) {
+            return res.status(400).json({error: 'invalid email or password'});
+        }
+ 
+         //find and compare users password
+        const passwordCheck = await bcrypt.compare(password, existingUser.password)
+
+        if (!passwordCheck) {
+            return res.status(400).json({ error: 'invalid email or password' });
+        }
+
+        const token = jsonwebtoken.sign({ id: existingUser.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+        res.status(200).json({ token: token });
+
+
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' })
+    }
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express'); 
 const {User, Holdings, sequelize} = require('./models/index')
 
