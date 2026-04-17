75bb399084393cb7a4da7bf3a04c04c81115ee6a
jessek26 <jesskooyenga@gmail.com>
2026-04-17 16:27:50 -0400

    added PostgreSQL support for production database

--- server/models/index.js
+++ server/models/index.js
@@ -2,10 +2,21 @@ const { Sequelize } = require('sequelize')
 const userModel = require('./User')
 const holdingsModel = require('./Holdings')
 
-const sequelize = new Sequelize({
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+    })
+    : new Sequelize({ 
     dialect: 'sqlite',
     storage: './database.sqlite'
-});
+    });
 
 const User = userModel(sequelize);
 const Holdings = holdingsModel(sequelize);
